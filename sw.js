d15020bf5efec0f072a01ee07447eb87f4c450a3
Bullethead <g6dzgch5yg@privaterelay.appleid.com>
2026-05-05 19:33:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'challenge-cards-v10';
+const CACHE = 'challenge-cards-v11';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -15,6 +15,7 @@ const ASSETS = [
   './js/data/positions.js',
   './js/data/truths.js',
   './js/data/config.js',
+  './js/data/library.js',
   // Components
   './js/components/DiceFace.js',
   // Main app
@@ -32,6 +33,19 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  if (url.pathname.includes('/content/')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
       const clone = res.clone();
